0a698b202b91ad8d11eeb1b8332fb2ad66b258fb
mdibella-dev <mdb@marcodibella.de>
2022-07-30 17:34:01 +0200

    Backport theme setup

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The theme's core file.
+ *
+ * @author  Marco Di Bella <mdb@marcodibella.de>
+ * @package mdb-theme-fse
+ */
+
+
+defined( 'ABSPATH' ) or exit;
+
+
+
+/** Turn off notices */
+
+error_reporting( E_ALL ^ E_NOTICE );
+
+
+/** Turn off development mode */
+
+define( 'MDB_DEV', true );
+
+
+/** Include function library */
+
+require_once( get_template_directory() . '/includes/setup.php' );
