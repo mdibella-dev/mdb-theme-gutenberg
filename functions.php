a6ea59e21dac9d3346293ed8becb16a2d0123e61
mdibella-dev <mdb@marcodibella.de>
2022-07-31 18:07:44 +0200

    Move favicon functions to separate file.

--- functions.php
+++ functions.php
@@ -25,3 +25,4 @@ define( 'MDB_DEV', true );
 
 require_once( get_template_directory() . '/includes/setup.php' );
 require_once( get_template_directory() . '/includes/block-patterns.php' );
+require_once( get_template_directory() . '/includes/favicon.php' );
