1b00fd483462632fdfabb45f03fdbbdf526163e3
Marco Di Bella <dibella@rechtsdepesche.de>
2022-08-10 16:31:08 +0200

    Save current working state

--- includes/patterns/scrollup.php
+++ includes/patterns/scrollup.php
@@ -15,12 +15,19 @@ return array(
     'title'    => __( 'Scroll up button', 'mdb' ),
     'inserter' => false,
     'content'  =>'
-<div id="scrollup" data-visible="false">
-    <button type="button" aria-label="' . __( 'To top', 'mdb' ) . '">
-        <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708
-            0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-        </svg>
-    </button>
-</div>'
+<!-- wp:group -->
+<div id="scrollup" class="wp-block-group" data-visible="false">
+
+<!-- wp:button -->
+<div class="wp-block-button">
+<a class="wp-block-button__link">
+<svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+<path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+</svg>
+</a>
+</div>
+<!-- /wp:button -->
+
+</div>
+<!-- /wp:group -->'
 );
