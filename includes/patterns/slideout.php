0aede824c96c1c422d22f461200d9bebd44e45ad
Marco Di Bella <dibella@rechtsdepesche.de>
2022-08-11 15:51:49 +0200

    Add slideout pattern (empty)

---
+++ includes/patterns/slideout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Block pattern: Slideout
+ *
+ * @author  Marco Di Bella
+ * @package mdb-theme-fse
+ */
+
+
+/** Prevent direct access */
+
+defined( 'ABSPATH' ) or exit;
+
+
+return array(
+    'title'    => __( 'Slideout navigation', 'mdb' ),
+    'inserter' => false,
+    'content'  =>''
+);
