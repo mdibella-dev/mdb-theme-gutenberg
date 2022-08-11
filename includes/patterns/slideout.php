45611a094f8473521516c10eeae4a5931d9a26da
mdibella-dev <mdb@marcodibella.de>
2022-08-11 20:20:40 +0200

    Add slideout pattern

--- includes/patterns/slideout.php
+++ includes/patterns/slideout.php
@@ -12,8 +12,43 @@
 defined( 'ABSPATH' ) or exit;
 
 
+
+/** Do the rendering */
+
+ob_start();
+
+?>
+<div id="slideout">
+<div id="slideout-content-wrapper">
+<div id="slideout-content">
+<?php
+wp_nav_menu( array(
+    'theme_location' => 'primary',
+    'container'      => 'nav',
+    'container_id'   => 'primary',
+) );
+?>
+<?php
+wp_nav_menu( array(
+    'theme_location' => 'secondary',
+    'container'      => 'nav',
+    'container_id'   => 'secondary',
+) );
+?>
+</div>
+</div>
+</div>
+<?php
+
+$content = ob_get_contents();
+ob_end_clean();
+
+
+
+/** Return the pattern */
+
 return array(
     'title'    => __( 'Slideout navigation', 'mdb' ),
     'inserter' => false,
-    'content'  =>''
+    'content'  =>'<!-- wp:html -->' . $content . '<!-- /wp:html -->'
 );
