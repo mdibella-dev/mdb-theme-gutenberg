1dcdce039986f7dc18f70fbb86696f8713528673
mdibella-dev <mdb@marcodibella.de>
2022-10-22 12:15:27 +0200

    Remove images folder and favicon.php

--- functions.php
+++ functions.php
@@ -31,5 +31,5 @@ require_once( get_template_directory() . '/includes/setup.php' );
 require_once( get_template_directory() . '/includes/block-patterns.php' );
 require_once( get_template_directory() . '/includes/block-styles.php' );
 require_once( get_template_directory() . '/includes/block-editor.php' );
-require_once( get_template_directory() . '/includes/favicon.php' );
+//require_once( get_template_directory() . '/includes/favicon.php' );
 require_once( get_template_directory() . '/includes/performance.php' );
