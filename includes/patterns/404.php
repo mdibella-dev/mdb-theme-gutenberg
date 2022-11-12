75f699b0a79adefccb446f880bc3f5d47560dec8
mdibella-dev <mdb@marcodibella.de>
2022-11-12 15:40:13 +0100

    Do the pattern rendering in 404.php in the same way like slideout.php.

--- includes/patterns/404.php
+++ includes/patterns/404.php
@@ -13,16 +13,29 @@ defined( 'ABSPATH' ) or exit;
 
 
 
+/** Do the rendering */
+
+ob_start();
+
+?>
+<!-- wp:group {"tagName":"section","className":"content"} -->
+<section class="wp-block-group content">
+<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-transparent marco-404 no-margin-top"} -->
+<figure class="wp-block-image size-full is-style-transparent marco-404 no-margin-top"><img src="<?php echo get_stylesheet_directory_uri(); ?>'/assets/images/marco-404.png"></figure>
+<!-- /wp:image -->
+</section>
+<!-- /wp:group -->'
+<?php
+
+$content = ob_get_contents();
+ob_end_clean();
+
+
+
 /** Return the pattern */
 
 return array(
     'title'    => __( '404', 'mdb' ),
     'inserter' => false,
-    'content'  =>'<!-- wp:group {"tagName":"section","className":"content"} -->
-<section class="wp-block-group content">
-<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-transparent marco-404 no-margin-top"} -->
-<figure class="wp-block-image size-full is-style-transparent marco-404 no-margin-top"><img src="' . get_stylesheet_directory_uri() . '/assets/images/marco-404.png"/></figure>
-<!-- /wp:image -->
-</section>
-<!-- /wp:group -->'
+    'content'  => $content
 );
