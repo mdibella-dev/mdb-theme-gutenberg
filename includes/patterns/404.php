591ef15114bc272d1bc22fef0f431a9dd1c87927
mdibella-dev <mdb@marcodibella.de>
2022-11-12 14:25:09 +0100

    Change path /img to /images

--- includes/patterns/404.php
+++ includes/patterns/404.php
@@ -21,7 +21,7 @@ return array(
     'content'  =>'<!-- wp:group {"tagName":"section","className":"content"} -->
 <section class="wp-block-group content">
 <!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-transparent marco-404 no-margin-top"} -->
-<figure class="wp-block-image size-full is-style-transparent marco-404 no-margin-top"><img src="' . get_stylesheet_directory_uri() . '/assets/img/marco-404.png"/></figure>
+<figure class="wp-block-image size-full is-style-transparent marco-404 no-margin-top"><img src="' . get_stylesheet_directory_uri() . '/assets/images/marco-404.png"/></figure>
 <!-- /wp:image -->
 </section>
 <!-- /wp:group -->'
