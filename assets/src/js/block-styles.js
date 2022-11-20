3a7b558cc8fe4bd35f96de937e6e4a3a04ea434c
mdibella-dev <mdb@marcodibella.de>
2022-11-20 13:38:53 +0100

    Remove several unused block variations
    There is currently no functioning plugin solution to disable block variations

--- assets/src/js/block-styles.js
+++ assets/src/js/block-styles.js
@@ -39,6 +39,36 @@ wp.domReady( () => {
         ]
     );
 
+    // Remove unused block variations
+    // Current block manager plugins are failing
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'instagram' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'soundcloud' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'spotify' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'flickr' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'animoto' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'cloudup' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'collegehumor' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'crowdsignal' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'imgur' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'kickstarter' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'mixcloud' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'reverbnation' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'screencast' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'scribd' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'speaker-deck' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'smugmug' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'tiktok' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'ted' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'tumblr' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'videopress' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'wordpress' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'wordpress-tv' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'amazon-kindle' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'wolfram-cloud' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'issuu' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'pocketcasts' );
+    wp.blocks.unregisterBlockVariation( 'core/embed', 'dailymotion' );
+
 
     /** core/heading */
 
