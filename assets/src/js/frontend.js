299a687263c5f0593708a840018a40bf63f9f97b
mdibella-dev <mdb@marcodibella.de>
2022-12-05 18:27:30 +0100

    Remove arrow symbol next to external links (#14, #3)

--- assets/src/js/frontend.js
+++ assets/src/js/frontend.js
@@ -66,17 +66,6 @@ jQuery( document ).ready( function( $ ) {
     } );
 
 
-    // Add a SVG icon to external anchors
-
-    $( '#main a[target="_blank"]' ).each( function() {
-        if( ! $( this ).hasClass( 'is-download' ) ) {
-            var arrow = '<svg aria-hidden="true" class="anchor-icon" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/></svg>';
-
-            $( this ).addClass( 'is-external' );
-            $( this ).wrapInner( '<span class="anchor-text"></span>').append( arrow );
-        }
-    } );
-
 
     // Add a 'to Top'-Button
     // The button only appears if at least half of the visible area (viewport) has been scrolled (threshold value).
