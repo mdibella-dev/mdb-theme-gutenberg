b38e0bf8dd6483a34922616cb239794376eba4c9
Marco Di Bella <mdb@marcodibella.de>
2022-12-09 13:34:14 +0100

    Strip title from download anchor

--- download-monitor/content-download-link.php
+++ download-monitor/content-download-link.php
@@ -13,7 +13,7 @@
 defined( 'ABSPATH' ) or exit;
 
 
-?><a class="download" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow" title="<?php echo __( 'Download file', 'mdb' ); ?>" target="_self">
+?><a class="download" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow" target="_self">
     <svg class="download-icon" fill="currentColor" viewBox="0 0 16 16">
         <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
     </svg>
