715e42183ae26dd4d155222815391192fd187b50
mdibella-dev <mdb@marcodibella.de>
2022-12-11 17:10:06 +0100

    Fix missing custom block styles in backend (#27)

--- includes/block-styles.php
+++ includes/block-styles.php
@@ -35,4 +35,4 @@ function register_block_styles()
     );
 }
 
-add_action( 'enqueue_block_editor_assets', 'mmdb_theme_fse\register_block_styles' );
+add_action( 'enqueue_block_editor_assets', 'mdb_theme_fse\register_block_styles' );
