f3d554ee347cc4576ecacd61e3e216057d81b940
Marco Di Bella <mdb@marcodibella.de>
2023-02-12 12:46:08 +0100

    Fix fatal error when using theme's consts

--- functions.php
+++ functions.php
@@ -23,16 +23,16 @@ error_reporting( E_ALL ^ E_NOTICE );
 
 /** Variables and definitions */
 
-define( __NAMESPACE__ . "THEME_VERSION", '1.4.0' );                 // The theme's version
-define( __NAMESPACE__ . "THEME_DIR", get_template_directory() );    // The theme's directory
+define( __NAMESPACE__ . "\THEME_VERSION", '1.4.0' );                       // The theme's version
+define( __NAMESPACE__ . "\THEME_DIR", get_template_directory() . '/' );    // The theme's directory
 
 
 
 /** Include files */
 
-require_once( THEME_DIR . '/includes/setup.php' );
-require_once( THEME_DIR . '/includes/block-patterns.php' );
-require_once( THEME_DIR . '/includes/block-styles.php' );
-require_once( THEME_DIR . '/includes/block-editor.php' );
-require_once( THEME_DIR . '/includes/performance.php' );
-require_once( THEME_DIR . '/includes/backend.php' );
+require_once( THEME_DIR . 'includes/setup.php' );
+require_once( THEME_DIR . 'includes/block-patterns.php' );
+require_once( THEME_DIR . 'includes/block-styles.php' );
+require_once( THEME_DIR . 'includes/block-editor.php' );
+require_once( THEME_DIR . 'includes/performance.php' );
+require_once( THEME_DIR . 'includes/backend.php' );
