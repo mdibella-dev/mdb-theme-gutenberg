fa4918b4220255da7ce0525745568f8c0ffb8a2e
Marco Di Bella <m_dibella@icloud.com>
2024-09-01 16:04:05 +0200

    Add new pattern

---
+++ patterns/related-posts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Related posts
+ * Slug: mdb-theme-fse/related-posts
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|80"}}}} -->
+    <h2 class="wp-block-heading has-text-align-center" style="margin-top:var(--wp--preset--spacing--80)"><?php echo __( 'Related posts', 'mdb-theme-fse' ); ?></h2>
+    <!-- /wp:heading -->
+    <!-- wp:mdb-theme-blocks/related-posts /-->
+</div>
+<!-- /wp:group -->
