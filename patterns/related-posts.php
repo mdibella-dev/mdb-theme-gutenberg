37306853e323cdff1bd2e2ec659f4c91c77d5be9
Marco Di Bella <m_dibella@icloud.com>
2024-09-08 19:10:48 +0200

    Fix margin and font-size

--- patterns/related-posts.php
+++ patterns/related-posts.php
@@ -7,8 +7,8 @@
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-    <!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|80"}}}} -->
-    <h2 class="wp-block-heading has-text-align-center" style="margin-top:var(--wp--preset--spacing--80)"><?php echo __( 'Related posts', 'mdb-theme-fse' ); ?></h2>
+    <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"2.88rem"}},"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|60"}}}} -->
+    <h2 class="wp-block-heading has-text-align-center" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--60);font-size:2.88rem"><?php echo __( 'Related posts', 'mdb-theme-fse' ); ?></h2>
     <!-- /wp:heading -->
     <!-- wp:mdb-theme-blocks/related-posts /-->
 </div>
