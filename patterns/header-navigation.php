c3d06e223e95aeb4e9d31061d72f23d808b8f24a
Marco Di Bella <m_dibella@icloud.com>
2024-09-08 18:00:26 +0200

    Update header-navigation.php

--- patterns/header-navigation.php
+++ patterns/header-navigation.php
@@ -9,8 +9,8 @@
 <div class="wp-block-group alignfull">
     <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"2em"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
     <div class="wp-block-group alignwide">
-        <!-- wp:paragraph {"align":"left","fontSize":"standard","style":{"typography":{"textTransform":"uppercase"}}} -->
-        <p class="has-text-align-left has-standard-font-size" style="font-size:1rem;text-transform:uppercase"><a href="/" rel="home">Marco <strong>Di Bella</strong></a></p>
+        <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","fontSize":"1rem"}}} -->
+        <p class="has-text-align-left" style="font-size:1rem;text-transform:uppercase"><a href="/" rel="home">Marco <strong>Di Bella</strong></a></p>
         <!-- /wp:paragraph -->
 
         <!-- wp:group {"className":"wp-group","layout":{"type":"flex","flexWrap":"nowrap"}} -->
