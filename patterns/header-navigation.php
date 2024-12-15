120642cb4b00c7271332fe60465c60138f85147f
Marco Di Bella <m_dibella@icloud.com>
2024-12-15 21:23:13 +0100

    Make font weight thicker to match with nav links

--- patterns/header-navigation.php
+++ patterns/header-navigation.php
@@ -9,7 +9,7 @@
 <div class="wp-block-group alignfull">
     <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"2em"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
     <div class="wp-block-group alignwide">
-        <!-- wp:site-title {"level":0,"style":{"typography":{"fontSize":"16px"}}} /-->
+        <!-- wp:site-title {"level":0,"style":{"typography":{"fontSize":"16px","fontWeight":"500"}}} /-->
 
         <!-- wp:group {"className":"wp-group","layout":{"type":"flex","flexWrap":"nowrap"}} -->
         <div class="wp-block-group wp-group">
