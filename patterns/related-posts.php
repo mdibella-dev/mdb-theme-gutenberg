e9833aa3e70411172efa9ef3bc6047e504ddbb12
Marco Di Bella <m_dibella@icloud.com>
2025-01-05 14:21:56 +0100

    Update related-posts.php

--- patterns/related-posts.php
+++ patterns/related-posts.php
@@ -8,8 +8,9 @@
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-    <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"xx-large","spacing":{"margin":{"top":"var(--wp--preset--spacing--80)","bottom":"var(--wp--preset--spacing--60)"}}} -->
-    <h2 class="wp-block-heading has-text-align-center has-xx-large-font-size" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--60);font-style:normal;font-weight:600;"><?php echo __( 'Related posts', 'mdb-theme-fse' ); ?></h2>
+
+    <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"3.5rem"}},"spacing":{"margin":{"top":"var(--wp--preset--spacing--80)","bottom":"var(--wp--preset--spacing--60)"}}} -->
+    <h2 class="wp-block-heading has-text-align-center" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--60);font-style:normal;font-weight:600;font-size:3.5rem"><?php echo __( 'Related posts', 'mdb-theme-fse' ); ?></h2>
     <!-- /wp:heading -->
 
     <!-- wp:mdb-theme-blocks/related-posts /-->
