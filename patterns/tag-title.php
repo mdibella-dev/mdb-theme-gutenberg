d61489310833134381c8cf0d8da8d043bffe387a
Marco Di Bella <m_dibella@icloud.com>
2025-04-21 13:10:26 +0200

    Change 'keyword' to 'topic'

--- patterns/tag-title.php
+++ patterns/tag-title.php
@@ -6,7 +6,7 @@
 ?>
 
 <!-- wp:paragraph {"align":"center","className":"is-style-tag-archive-kicker","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
-<p class="has-text-align-center is-style-tag-archive-kicker" style="margin-top:var(--wp--preset--spacing--40);"><?php echo __( 'Keyword', 'mdb-theme-fse' ); ?></p>
+<p class="has-text-align-center is-style-tag-archive-kicker" style="margin-top:var(--wp--preset--spacing--40);"><?php echo __( 'Topic', 'mdb-theme-fse' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:query-title {"type":"archive","textAlign":"center","showPrefix":false,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|80"}}}} /-->
